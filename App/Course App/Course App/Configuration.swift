daabfe2c01662292384844cec7ff1ac74595ee9f
Patrik Urban <patrikurban.work@gmail.com>
2024-05-11 00:32:50 +0200

    Add all step for for homework 04

---
+++ App/Course App/Course App/Configuration.swift
@@ -0,0 +1,38 @@
+//
+//  Configuration.swift
+//  Course App
+//
+//  Created by Patrik Urban on 10.05.2024.
+//
+
+import Foundation
+
+struct Configuration: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case apiJokesBaseURL = "API_JOKES_BASE_URL"
+        case apiImagesBaseURL = "API_IMAGES_BASE_URL"
+    }
+    
+    let apiJokesBaseURL: URL
+    let apiImagesBaseURL: URL
+}
+
+extension Configuration {
+    static let `default`: Configuration = {
+        guard let propertyList = Bundle.main.infoDictionary else {
+            fatalError("Unable to get property list.")
+        }
+
+        guard let data = try? JSONSerialization.data(withJSONObject: propertyList, options: []) else {
+            fatalError("Unable to instantiate data from property list.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let configuration = try? decoder.decode(Configuration.self, from: data) else {
+            fatalError("Unable to decode the Environment configuration file.")
+        }
+
+        return configuration
+    }()
+}
---
+++ App/Course App/Course App/Course_AppApp.swift
@@ -0,0 +1,18 @@
+//
+//  Course_AppApp.swift
+//  Course App
+//
+//  Created by Patrik Urban on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+// swiftlint:disable:next type_name
+struct Course_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
